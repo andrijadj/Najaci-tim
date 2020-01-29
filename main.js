942d99592b88151d68637c12b96cb8aacc14a433
andrijadj <andrijadjordjevic011@gmail.com>
2020-01-29 20:32:17 +0100

    Radite dalje

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Ajde prikljucite se!");
+class Car {
+    constructor(motor, gume) {
+        this.motor = motor;
+        this.gume = gume;
+    }
+
+    potrosnjaGoriva() {
+        console.log("Automobil tipa "+this.motor+"je najprodavaniji model ove godine, i dolazi u paketu sa 4 "+this.gume+"gume.")
+    }
+
+}
+

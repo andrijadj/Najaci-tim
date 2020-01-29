94c83b9e01fa74acff4fb2a1f98f88b4e3fb90f3
code-edu <test.workshop@hotmail.com>
2020-01-29 20:46:00 +0100

    comitt

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Ajde prikljucite se!");
+class Renault extends Car {
+  constructor(motor, gume, model, boja, tip, gorivo) {
+    super(motor,gume);
+    this.model = model;
+    this.boja = boja;
+    this.tip = tip;
+    this.gorivo = gorivo;
+  }
+  
+  proveraKilometraze() {
+    const km = 100*Math.random().toFixed();
+    console.log('Predjena kilometraza automobila je: '+km);
+  }
+}
